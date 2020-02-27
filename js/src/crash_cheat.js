554223c338ab32f0c3a9b98a9b78e6b3060c7541
kuixing233 <2609051217@qq.com>
2020-02-27 21:28:41 +0800

    Site updated: 2020-02-27 21:28:40

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,15 +1,15 @@
-<!--������ƭ-->
+<!--崩溃欺骗-->
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '�q(��A��`)�r ҳ������� ~';
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(?>��<*?) ���ֺ���~' + OriginTitle;
+         document.title = '(?>ω<*?) 噫又好了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
